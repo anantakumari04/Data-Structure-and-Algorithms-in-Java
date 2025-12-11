94faf4a22822939023ed1ea8e76b9e9c7ed9e5b0
anantakumari04 <kumariananta01@gmail.com>
2025-12-11 15:52:37 +0530

    Java DSA

---
+++ src/Arrays/IfArrayisSorted.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class IfArrayisSorted {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of an array: ");
+
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements in an array: ");
+
+        int arr[]= new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        boolean flag = false;
+
+        for(int i = 1; i<n; i++){
+            if(arr[i]>=arr[i-1]){
+                flag = true;
+            }
+            else{
+                flag = false;
+                break;
+            }
+
+
+        }
+
+        if(flag){
+            System.out.println("Array is sorted");
+        }
+        else
+            System.out.println("Array is not sorted");
+
+    }
+}
